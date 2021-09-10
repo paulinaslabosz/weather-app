4546ab4daf63e9ca09da26a8b817622f3c25c8c8
Paulina Słabosz <sejmipool@gmail.com>
2021-09-10 22:17:13 +0200

    feature: getting weather from api

--- main.js
+++ main.js
@@ -4,5 +4,62 @@ const button = document.querySelector('button');
 const warn = document.querySelector('.warning');
 const image = document.querySelector('.photo')
 const weather = document.querySelector('.weather')
-const temp = document.querySelector('.temp')
+const temperature = document.querySelector('.temp')
 const humidity = document.querySelector('.humidity')
+
+const link = 'https://api.openweathermap.org/data/2.5/weather?q='
+const key = '&appid=ff66fa752296c3ea3f5605e8f62c6086'
+const metric = '&units=metric'
+
+
+let city 
+let url
+
+
+
+const getWeather = () =>
+{
+
+ // city = input.value;
+ city = 'London'
+ url = link + city + key + metric
+ 
+ axios.get(url) 
+ .then (res =>{
+         const temp = Math.floor(res.data.main.temp);
+     const hum = res.data.main.humidity;
+     const status = Object.assign({}, ...res.data.weather);
+     
+
+     cityName.textContent = res.data.name;
+     temperature.textContent = temp + '°C';
+     humidity.textContent = hum + '%';
+     weather.textContent = status.main;
+     console.log(status);
+
+     if (status.id >= 200 && status.id < 300) {
+         image.setAttribute('src', '/images/thunderstorm.png')
+     } else if (status.id >= 300 && status.id < 500) {
+        image.setAttribute('src', '/images/drizzle.png')
+     } else if (status.id >= 500 && status.id < 600) {
+        image.setAttribute('src', '/images/rain.png')
+     } else if (status.id >= 600 && status.id < 700) {
+        image.setAttribute('src', '/images/ice.png')
+     } else if (status.id >= 701 && status.id < 800) {
+        image.setAttribute('src', '/images/fog.png')
+     } else if (status.id === 800) {
+        image.setAttribute('src', '/images/sun.png')
+     } else if (status.id >= 801 && status.id < 805) {
+        image.setAttribute('src', '/images/cloud.png')
+     } else {
+         image.setAttribute('src', '/image/unknown.png')
+     }
+
+
+
+    })
+}
+
+
+
+getWeather()
